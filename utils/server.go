d3dd574af9e632e2fb526a4c20691c99f03c72e9
Amaymon <mambetaliev.02@mail.ru>
2022-07-03 15:50:39 +0600

    added: more commits

--- utils/server.go
+++ utils/server.go
@@ -51,32 +51,45 @@ func (s *Server) ConnectMessenger(conn net.Conn) {
 		log.Printf("ConnectMessenger: %v\n", err.Error())
 		return
 	}
-	// 
+	// получаем имя
 	name = strings.Replace(name, "\n", "", 1)
+	// добавляем юзера в сервер
 	err = s.addConnection(conn, name)
 	if err != nil {
 		fmt.Fprint(conn, err.Error())
 		conn.Close()
 		return
 	}
+	// начинаем общение
 	s.StartChating(conn)
+	// удаляем соединение если пользователь нажал на ctrl c
 	s.RemoveConnection(conn)
 }
 //StartChating функция которая выгружает все смс отправленные до пользователя и добавляет его самого в чат
 func (s *Server) StartChating(conn net.Conn) {
+	// загружаем все письма предыдущих пользователей к новому пользователю
 	s.DownloadMessages(conn)
+	// готовим сообщение о том что новый пользователь присоединился
 	sms := getFormattedMessage(s, conn, "", ModeJoinChat)
+	// отправляем в общий чат
 	s.SendMessage(conn, sms)
+	// запускаем бесконечный цикл который слушает что напишет пользователь постоянно
 	for {
+		// создаёт поле для чтения
 		message, err := bufio.NewReader(conn).ReadString('\n')
 		if err != nil {
 			break
 		}
+		// готовит сообщение к отправке
 		message = getFormattedMessage(s, conn, message, ModeSendMessage)
+		// отправляем это сообщение
 		s.SendMessage(conn, message)
+		// сохраняет это сообщение
 		s.SavedMessage(message)
 	}
+	// если он нажал ctrl c то срабатывает if внутри цикла и мы переходим к это строчке кода которое готовит сообщение о том что пользователь вышел
 	message := getFormattedMessage(s, conn ,"", ModeLeftChat)
+	// отправляем это сообщение в группу 
 	s.SendMessage(conn, message)
 }
 
@@ -88,6 +101,7 @@ func (s *Server) SavedMessage(message string) {
 }
 //SendMessage отправляет всем юзерам на сервере
 func (s *Server) SendMessage(conn net.Conn, message string) {
+	// проверяем на пустоту
 	if message == "" {
 		fmt.Fprintf(conn, PatternSending, time.Now().Format(TimeDefault), s.Connections[conn])
 		return
@@ -107,6 +121,9 @@ func (s *Server) SendMessage(conn net.Conn, message string) {
 
 func getFormattedMessage(srv *Server, conn net.Conn, message string, mode int) string {
 	srv.mutex.Lock()
+	// лочим поток
+
+	// получаем имя пользователя
 	name := srv.Connections[conn]
 	srv.mutex.Unlock()
 	// Change Message
@@ -116,28 +133,40 @@ func getFormattedMessage(srv *Server, conn net.Conn, message string, mode int) s
 			return ""
 		}
 		time := time.Now().Format(TimeDefault)
+		// в зависимости от mode отправляем сообщение это вывод обычного сообщения
 		message = fmt.Sprintf(PatternMessage, time, name, message)
 	case ModeJoinChat:
+		// уведомляем о соединении нового юзера
 		message = fmt.Sprintf(ColorYellow+PatternJoinChat+ColorReset, name)
 	case ModeLeftChat:
+		// уведомляем о выходе пользователя
 		message = fmt.Sprintf(ColorYellow+PatternLeftChat+ColorReset, name)
 	}
+	// отправляем готовое сообщение
 	return message
 }
 
 func (s *Server) addConnection(conn net.Conn, name string) error {
+	// проверяем имя на пустоту
 	if name == "" {
 		return errors.New("Name cant be empty")
 	}
+	// блокируем доступ другим горутинам
 	s.mutex.Lock()
+	//  после завершения функции открываем обратно
 	defer s.mutex.Unlock()
+	// проверяем может ли юзер зайти в систему
 	if s.MaxConnections != 0 && len(s.Connections) >= s.MaxConnections {
 		return fmt.Errorf("the room is full (%v)", conn.RemoteAddr())
 	} else if s.UsedNames[name] {
+		//  проверка есть ли такое имя уже в системе
 		return fmt.Errorf("name '%s' is exist [%v] ", name, conn.RemoteAddr())
 	}
+	// добавляем имя в общую структуру
 	s.UsedNames[name] = true
+	// добавляем в мапу коннект этого юзера и его имя в значении
 	s.Connections[conn] = name
+	// уведомляем пользователей о вхождении в систему
 	log.Printf("Connect %v ", conn.RemoteAddr())
 	return nil
 }
@@ -152,11 +181,14 @@ func (s *Server) DownloadMessages(conn net.Conn) {
 
 // RemoveConnection - Removing Connection from s.Connections (safe)
 func (s *Server) RemoveConnection(conn net.Conn) {
+	// лочим поток от других горутин
 	s.mutex.Lock()
 	// удаляем указанный элемент с мапы
 	delete(s.UsedNames, s.Connections[conn])
+	// удаляем указанный элемент с другой мапы
 	delete(s.Connections, conn)
 	// уведомляем пользователей о том что юзер вышел из чата
 	log.Printf("Connect %v was left", conn.RemoteAddr())
+	// открываем соединение для других горутин
 	s.mutex.Unlock()
 }
